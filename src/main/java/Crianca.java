210b39e2d0dd9ba238cc080fa2f7ffc0d4a00025
kaiop00 <kaioportela@alu.ufc.br>
2021-08-22 22:29:16 -0300

    pronto

---
+++ src/main/java/Crianca.java
@@ -0,0 +1,13 @@
+public class Crianca {
+
+    public Crianca(String name, int idade){
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getIdade() {
+        return 99;
+    }
+}
---
+++ src/main/java/PulaPula.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PulaPula {
+
+
+    public PulaPula(int limiteMax){
+    }
+
+    public List<Crianca> getFilaDeEspera() {
+        return null;
+    }
+
+    public List<Crianca> getCriancasPulando() {
+        return null;
+    }
+
+    public int getLimiteMax() {
+        return -1;
+    }
+
+    public double getCaixa() {
+        return -1;
+    }
+
+    public Double getConta(String name){
+        return null;
+    }
+
+    public boolean entrarNaFila(Crianca crianca){
+        return true;
+    }
+
+    public boolean entrar(){
+        return true;
+    }
+
+    public boolean sair(){
+        return true;
+    }
+
+    public boolean papaiChegou(String name){
+        return false;
+    }
+
+    public double fechar(){
+       return -1;
+    }
+}
